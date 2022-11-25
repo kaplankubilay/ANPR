ff3c4047a650ed8afed5a4a287ebb1627865528d
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2022-11-25 21:25:14 +0300

    Add project files.

---
+++ ANPR/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace ANPR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.baslatButon = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(318, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(335, 52);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kameralar";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(92, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(201, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(618, 491);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // baslatButon
+            // 
+            this.baslatButon.Location = new System.Drawing.Point(35, 13);
+            this.baslatButon.Name = "baslatButon";
+            this.baslatButon.Size = new System.Drawing.Size(97, 38);
+            this.baslatButon.TabIndex = 5;
+            this.baslatButon.Text = "Başlat";
+            this.baslatButon.UseVisualStyleBackColor = true;
+            this.baslatButon.Click += new System.EventHandler(this.baslatButon_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 623);
+            this.Controls.Add(this.baslatButon);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button baslatButon;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ANPR/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+
+namespace ANPR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ANPRDBEntities _dataBase = new ANPRDBEntities();
+        List<PlateRecognition> _plateList = new List<PlateRecognition>();
+
+        private FilterInfoCollection fico;
+        private VideoCaptureDevice vcd;
+
+        private void toTextButton_Click(object sender, EventArgs e)
+        {
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    var image = new Bitmap(openFileDialog1.FileName);
+            //    var ocr = new TesseractEngine("./Traineddata", "eng");
+            //    var sonuc = ocr.Process(image);
+            //    richTextBox1.Text = sonuc.GetText();
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fico = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo item in fico)
+            {
+                comboBox1.Items.Add(item.Name);
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void baslatButon_Click(object sender, EventArgs e)
+        {
+            vcd = new VideoCaptureDevice(fico[comboBox1.SelectedIndex].MonikerString);
+            vcd.NewFrame += Vcd_NewFrame; //hareketli görüntü sağlar
+            vcd.Start();
+            timer1.Start();
+            _plateList = _dataBase.PlateRecognition.ToList();
+        }
+
+        /// <summary>
+        /// kameradan alınan çerçeve aktarıldı.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void Vcd_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void yakalaButon_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog sfd = new SaveFileDialog();
+            //sfd.Filter = "(*.jpg)|*.jpg";
+            //DialogResult dr = sfd.ShowDialog();
+
+
+            //if (dr == DialogResult.OK)//diyalog ok ise resmi kaydet
+            //{
+            //    pictureBox1.Image.Save(sfd.FileName);
+            //}
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                var image = new Bitmap(pictureBox1.Image);
+                var ocr = new TesseractEngine("./Traineddata", "eng");
+                var sonuc = ocr.Process(image);
+                if (sonuc != null)
+                {
+                    string readyText = RemoveSpaceUpper(sonuc.GetText());
+
+                    string result = CleanTextAlgorithm(readyText);
+
+                    if (_plateList.Any(x=>x.Plate == result))
+                    {
+                        richTextBox1.Text = result;
+                        var asd=_plateList.FirstOrDefault(x => x.Plate == result);
+                        asd.CreateDate=DateTime.Now;
+                        _dataBase.SaveChanges();
+                    }
+                }
+            }
+        }
+
+        private string CleanTextAlgorithm(string readyText)
+        {
+            try
+            {
+                int count = readyText.Length;
+                string result = "";
+
+                for (int i = 0; i < count; i++)
+                {
+                    if (readyText[i] == '/')
+                    {
+                        i++;
+                        continue;
+                    }
+
+                    result += readyText[i];
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        private static string RemoveSpaceUpper(string input)
+        {
+            try
+            {
+                return new string(input.ToCharArray()
+                    .Where(c => !Char.IsWhiteSpace(c))
+                    .ToArray()).ToUpper();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
