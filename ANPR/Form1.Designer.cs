0a8817acd4c5ea0b7539555d773f781b98f208a4
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2023-01-03 22:34:59 +0300

    görsel iyileştirmeler eklendi.

--- ANPR/Form1.Designer.cs
+++ ANPR/Form1.Designer.cs
@@ -38,14 +38,19 @@ namespace ANPR
             this.baslatButon = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.plaka_lbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yenile_btn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(493, 26);
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(363, 43);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(160, 52);
+            this.richTextBox1.Size = new System.Drawing.Size(267, 74);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
@@ -56,37 +61,40 @@ namespace ANPR
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 60);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 55);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.Size = new System.Drawing.Size(122, 26);
             this.label1.TabIndex = 2;
             this.label1.Text = "Kameralar";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(92, 57);
+            this.comboBox1.Location = new System.Drawing.Point(170, 60);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(201, 21);
+            this.comboBox1.Size = new System.Drawing.Size(168, 21);
             this.comboBox1.TabIndex = 3;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(35, 98);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 123);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(618, 491);
+            this.pictureBox1.Size = new System.Drawing.Size(618, 424);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // baslatButon
             // 
-            this.baslatButon.Location = new System.Drawing.Point(35, 13);
+            this.baslatButon.BackColor = System.Drawing.Color.GreenYellow;
+            this.baslatButon.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.baslatButon.Location = new System.Drawing.Point(12, 9);
             this.baslatButon.Name = "baslatButon";
-            this.baslatButon.Size = new System.Drawing.Size(97, 38);
+            this.baslatButon.Size = new System.Drawing.Size(160, 43);
             this.baslatButon.TabIndex = 5;
             this.baslatButon.Text = "Başlat";
-            this.baslatButon.UseVisualStyleBackColor = true;
+            this.baslatButon.UseVisualStyleBackColor = false;
             this.baslatButon.Click += new System.EventHandler(this.baslatButon_Click);
             // 
             // timer1
@@ -96,17 +104,64 @@ namespace ANPR
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(313, 26);
+            this.richTextBox2.Location = new System.Drawing.Point(244, 91);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(163, 52);
+            this.richTextBox2.Size = new System.Drawing.Size(94, 21);
             this.richTextBox2.TabIndex = 6;
             this.richTextBox2.Text = "";
+            this.richTextBox2.Visible = false;
+            // 
+            // plaka_lbl
+            // 
+            this.plaka_lbl.AutoSize = true;
+            this.plaka_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.plaka_lbl.Location = new System.Drawing.Point(363, 9);
+            this.plaka_lbl.Name = "plaka_lbl";
+            this.plaka_lbl.Size = new System.Drawing.Size(176, 26);
+            this.plaka_lbl.TabIndex = 7;
+            this.plaka_lbl.Text = "GELEN PLAKA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(7, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "GELEN ARAÇ";
+            // 
+            // yenile_btn
+            // 
+            this.yenile_btn.BackColor = System.Drawing.Color.Yellow;
+            this.yenile_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.yenile_btn.Location = new System.Drawing.Point(178, 9);
+            this.yenile_btn.Name = "yenile_btn";
+            this.yenile_btn.Size = new System.Drawing.Size(160, 43);
+            this.yenile_btn.TabIndex = 9;
+            this.yenile_btn.Text = "Yenile";
+            this.yenile_btn.UseVisualStyleBackColor = false;
+            this.yenile_btn.Click += new System.EventHandler(this.yenile_btn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(9, 550);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "@Created By BİTEG TECHNOLOGY 2023";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 623);
+            this.ClientSize = new System.Drawing.Size(636, 567);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.yenile_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.plaka_lbl);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.baslatButon);
             this.Controls.Add(this.pictureBox1);
@@ -114,7 +169,7 @@ namespace ANPR
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Gerçek Zamanlı Plaka Okuma Uygulaması";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -131,6 +186,10 @@ namespace ANPR
         private System.Windows.Forms.Button baslatButon;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label plaka_lbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button yenile_btn;
+        private System.Windows.Forms.Label label3;
     }
 }
 
